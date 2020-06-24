412729bd17353f68389201218ae19b0b1826df36
Do1phin <Do1phin@users.noreply.github.com>
2020-06-24 19:22:34 +0300

    Hotfix (#39)

--- helpers/dbErrorHandler.js
+++ helpers/dbErrorHandler.js
@@ -1,9 +1,11 @@
 const getErrorMessage = (error) => {
 
     if (error.name === 'MongoError') {
-        throw new Error(`${error.name}: code [${error.code}] - ${error.errmsg}`)
+        // throw new Error(`${error.name}: code [${error.code}] - ${error.errmsg}`)
+        console.error(`${error.name}: code [${error.code}] - ${error.errmsg}`);
     } else {
-        throw new Error(`Ошибка ${error}`);
+        // throw new Error(`Ошибка ${error}`);
+        console.error(`Ошибка ${error}`);
     }
 };
 
